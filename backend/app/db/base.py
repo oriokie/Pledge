0849f5f813f58659540cf82cb7c8cb96dc977c10
Edwin <oriokie@icloud.com>
2025-03-23 22:24:56 +0300

    deleted all

--- backend/app/db/base.py
+++
@@ -1,6 +0,0 @@
-from app.db.base_class import Base
-from app.models.user import User
-from app.models.member import Member
-from app.models.group import Group
-from app.models.project import Project
-from app.models.contribution import Contribution 
